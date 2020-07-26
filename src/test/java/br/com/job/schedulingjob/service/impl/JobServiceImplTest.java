90416c5c05e0454830b3f13b571e05f88d2a18e8
Marcelo S Estrela <marcelosouzajob@gmail.com>
2020-07-26 17:45:59 -0300

    Testes unitarios - adicionado mais 1 caso de testes

--- src/test/java/br/com/job/schedulingjob/service/impl/JobServiceImplTest.java
+++ src/test/java/br/com/job/schedulingjob/service/impl/JobServiceImplTest.java
@@ -68,6 +68,20 @@ public class JobServiceImplTest {
 		assertThat(retorno).isEqualTo(esperado);
 	}
 	
+	@Test
+	public void definirJobsExecucao_testCase3() {
+		List<List<Integer>> esperado = 
+				Arrays.asList(Arrays.asList(Integer.valueOf(6), Integer.valueOf(3)),
+					          Arrays.asList(Integer.valueOf(1)));
+		
+		JobsExecucao jobsExecucao = inputTestCase3();
+		List<List<Integer>> retorno = jobService.definirJobsExecucao(jobsExecucao.getJanelaExecucaoInicio(),
+				                                                     jobsExecucao.getJanelaExecucaoFinal(),
+				                                                     jobsExecucao.getJobsExecucao());
+		
+		assertThat(retorno).isEqualTo(esperado);
+	}
+	
 	private JobsExecucao inputTestCase1() {
 		String json = "{\r\n" + 
 				"    \"janelaExecucaoInicio\": \"2019-11-10T09:00:00\",\r\n" + 
@@ -102,6 +116,53 @@ public class JobServiceImplTest {
 		String json = "{\r\n" + 
 				"    \"janelaExecucaoInicio\": \"2019-11-10T09:00:00\",\r\n" + 
 				"    \"janelaExecucaoFinal\":  \"2019-11-11T12:00:00\",\r\n" + 
+				"    \"jobsExecucao\": [\r\n" + 
+				"        {\r\n" + 
+				"            \"id\": 1,\r\n" + 
+				"            \"descricao\": \"Importação de arquivos de fundos\",\r\n" + 
+				"            \"dataMaximaConclusao\": \"2019-11-10T20:00:00\",\r\n" + 
+				"            \"tempoEstimadoExecucao\": 2\r\n" + 
+				"        },\r\n" + 
+				"        {\r\n" + 
+				"            \"id\": 2,\r\n" + 
+				"            \"descricao\": \"Importação de dados da Base Legada\",\r\n" + 
+				"            \"dataMaximaConclusao\": \"2019-11-10T23:00:00\",\r\n" + 
+				"            \"tempoEstimadoExecucao\": 5\r\n" + 
+				"        },\r\n" + 
+				"        {\r\n" + 
+				"            \"id\": 3,\r\n" + 
+				"            \"descricao\": \"Importação de dados da Base Legada\",\r\n" + 
+				"            \"dataMaximaConclusao\": \"2019-11-10T17:00:00\",\r\n" + 
+				"            \"tempoEstimadoExecucao\": 4\r\n" + 
+				"        },\r\n" + 
+				"        {\r\n" + 
+				"            \"id\": 4,\r\n" + 
+				"            \"descricao\": \"Importação de dados da Base Legada\",\r\n" + 
+				"            \"dataMaximaConclusao\": \"2019-11-10T11:00:00\",\r\n" + 
+				"            \"tempoEstimadoExecucao\": 3\r\n" + 
+				"        },\r\n" + 
+				"        {\r\n" + 
+				"            \"id\": 5,\r\n" + 
+				"            \"descricao\": \"Importação de dados da Base Legada\",\r\n" + 
+				"            \"dataMaximaConclusao\": \"2019-11-10T22:00:00\",\r\n" + 
+				"            \"tempoEstimadoExecucao\": 6\r\n" + 
+				"        },\r\n" + 
+				"        {\r\n" + 
+				"            \"id\": 6,\r\n" + 
+				"            \"descricao\": \"Importação de dados de integração\",\r\n" + 
+				"            \"dataMaximaConclusao\": \"2019-11-10T12:00:00\",\r\n" + 
+				"            \"tempoEstimadoExecucao\": 3\r\n" + 
+				"        }\r\n" + 
+				"    ]\r\n" + 
+				"}";
+		
+        return parseJsonToObject(json);
+	}
+	
+	private JobsExecucao inputTestCase3() {
+		String json = "{\r\n" + 
+				"    \"janelaExecucaoInicio\": \"2019-11-10T09:00:00\",\r\n" + 
+				"    \"janelaExecucaoFinal\":  \"2019-11-10T22:00:00\",\r\n" + 
 				"    \"jobsExecucao\": [\r\n" + 
 				"        {\r\n" + 
 				"            \"id\": 1,\r\n" + 
