62426acb127f307408330fb6f7667124f729d418
Marcelo S Estrela <marcelosouzajob@gmail.com>
2020-07-25 21:36:48 -0300

    Config inicial - Spring boot e web para utilizacao de endpoints

---
+++ src/main/java/br/com/job/schedulingjob/SchedulingJobApplication.java
@@ -0,0 +1,13 @@
+package br.com.job.schedulingjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/job/schedulingjob/SchedulingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.job.schedulingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
